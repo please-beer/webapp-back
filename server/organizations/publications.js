81b21bc35cc26d8045aceb7c6aee67c7aea1a7e4
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-03-22 23:12:29 +0100

    Added publication for owned organizations

--- server/organizations/publications.js
+++ server/organizations/publications.js
@@ -2,3 +2,7 @@ Meteor.publish("organizations:byId", function (_id) {
     check(_id, String);
     return Organizations.find({_id: _id});
 });
+
+Meteor.publish("organizations:owned", function () {
+    return Organizations.find({owner: this.userId});
+});

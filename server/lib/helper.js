cc5dc0f82c139e8b397c92f020fbf041d0261c89
Dariusz Tarczynski <dariusz.tarczynski@gmail.com>
2016-01-03 22:52:50 +0100

    added proper login token to Stripe service method calls

--- server/lib/helper.js
+++ server/lib/helper.js
@@ -7,47 +7,51 @@ Meteor.methods({
   "sendLogMessage": function(){
         return "Hello world";
     },
-  "user:stripe-sync": function(){
+  "user:stripe-sync": function(loginToken){
   	try {
-	return HTTP.post("http://localhost:4000/call/", {data:{"method": "user:stripe-sync","params":[],"loginToken":"DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23"}});
+	return HTTP.post("http://localhost:4000/call/", {data:{"method": "user:stripe-sync","params":[],"loginToken":loginToken}});
 	} catch(error) {
 	 return error;
 	}
   },
-  "user:stripe-login": function(){
+  "user:stripe-login": function(loginToken){
   	try {
-	return HTTP.post("http://localhost:4000/call/", {data:{"method": "user:stripe-login","params":[],"loginToken":"DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23"}});
+	return HTTP.post("http://localhost:4000/call/", {data:{"method": "user:stripe-login","params":[],"loginToken":loginToken}});
 	} catch(error) {
 	 return error;
 	}
   },
-  "users:add-card": function(exp_month,exp_year,number){
+  "users:add-card": function(exp_month,exp_year,number, loginToken){
   	try {
-	return HTTP.post("http://localhost:4000/call/", {data:{"method": "users:add-card","params":[{"exp_month":exp_month, "exp_year":exp_year,"number":number}],"loginToken":"DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23"}});
+	return HTTP.post("http://localhost:4000/call/", {data:{"method": "users:add-card","params":[{"exp_month":exp_month, "exp_year":exp_year,"number":number}],"loginToken":loginToken}});
 	} catch(error) {
 	 return error;
 	}
   },
-  "users:delete-card": function(cardID){
+  "users:delete-card": function(cardID, loginToken){
   	try {
-	return HTTP.post("http://localhost:4000/call/", {data:{"method": "users:delete-card","params":[{"cardID":cardID}],"loginToken":"DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23"}});
+	return HTTP.post("http://localhost:4000/call/", {data:{"method": "users:delete-card","params":[{"cardID":cardID}],"loginToken":loginToken}});
 	} catch(error) {
 	 return error;
 	}
   },
-  "users:list-cards": function(token){
-  	try {//Accounts._storedLoginToken(
-  		if(!token) token = "DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23";
+  "users:list-cards": function(loginToken){
   		console.log("Function users:list-cards");
-	return HTTP.post("http://localhost:4000/call/", {data:{"method": "users:list-cards","params":[],"loginToken":token}});
-	} catch(error) {
-	 return error;
-	}
+  		var f = new Future();
+	    HTTP.post("http://localhost:4000/call/", {data:{"method": "users:list-cards","params":[],"loginToken":loginToken}}, function(error, result) {
+    		console.log(result);
+	    	console.log(error);
+	    	if (result)
+	    	{
+	    		f.return(result);
+	    	} else f.throw(error);
+	    });
+	    return f.wait();
   },
-    "users:pay-for-beer": function(campaign, price, title, description, card){
+    "users:pay-for-beer": function(campaign, price, title, description, card, loginToken){
   		console.log("Function users:pay-for-beer");
   		var f = new Future();
-		HTTP.post("http://localhost:4000/call/", {data:{"method": "users:submit-payment","params":[{"campaignID":campaign, "ammount":price, "title":title, "description":description, "source":card}],"loginToken":"DpksFEBuVdwY_QkAYb9jxCX_o2DiBXPjyZsTZyRMS23"}}, function(error, result) {
+		HTTP.post("http://localhost:4000/call/", {data:{"method": "users:submit-payment","params":[{"campaignID":campaign, "ammount":price, "title":title, "description":description, "source":card}],"loginToken":loginToken}}, function(error, result) {
         if (result)
         {
 	        Meteor.call('sendPaymentConfirmatinoEmail', title, description, price);
